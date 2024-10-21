609c17fde84c49bd484c66d64d2bf9bba55889a4
f4rral <f4rral@gmail.com>
2024-10-21 22:55:15 +0300

    Logs TaskItemAdapter

--- app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
+++ app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -1,5 +1,6 @@
 package com.hfad.tasks
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -21,12 +22,16 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
     // Это тот момент, когда создаётся layout строки списка, передается объекту ViewHolder,
     // и каждый дочерний view-компонент может быть найден и сохранен.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
+        Log.i("MyLog", "onCreateViewHolder")
+
         return TaskItemViewHolder.inflateFrom(parent)
     }
 
     // Принимает объект ViewHolder и устанавливает необходимые данные
     // для соответствующей строки во view-компоненте.
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        Log.i("MyLog", "onBindViewHolder")
+
         val item = data[position]
         holder.bind(item)
     }
@@ -36,6 +41,7 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
 
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
+                Log.i("MyLog", "TaskItemViewHolder inflateFrom")
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
 
@@ -44,6 +50,8 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
         }
 
         fun bind(item: Task) {
+            Log.i("MyLog", "TaskItemViewHolder bind")
+
             rootView.text = item.taskName
         }
     }
